d9730fc3f175383a9962689e8fde0e68492859cd
sangmo-land <sangmolandry12@gmail.com>
2026-01-07 17:41:48 +0100

    AdminAccess

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,12 +6,18 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Panel;
 
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
 
+public function canAccessPanel(Panel $panel): bool
+    {
+    return (bool) $this->is_admin;
+    }
     /**
      * The attributes that are mass assignable.
      *
@@ -21,6 +27,7 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+'is_admin',
     ];
 
     /**
