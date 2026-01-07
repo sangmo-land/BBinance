7e6e6cb979474e11aef16c5ee9781c606f5eab3b
sangmo-land <sangmolandry12@gmail.com>
2026-01-07 18:27:02 +0100

    UserTableAdmin

---
+++ app/Filament/Resources/Users/Schemas/UserForm.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Filament\Resources\Users\Schemas;
+
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\Toggle;
+use Filament\Schemas\Schema;
+use Illuminate\Support\Facades\Hash;
+
+class UserForm
+{
+    public static function configure(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                TextInput::make('name')
+                    ->required()
+                    ->maxLength(255),
+                TextInput::make('email')
+                    ->email()
+                    ->required()
+                    ->maxLength(255),
+                TextInput::make('password')
+                    ->password()
+                    ->dehydrateStateUsing(fn ($state) => Hash::make($state))
+                    ->dehydrated(fn ($state) => filled($state))
+                    ->required(fn (string $context): bool => $context === 'create'),
+                Toggle::make('is_admin')
+                    ->label('Admin')
+                    ->required(),
+            ]);
+    }
+}
