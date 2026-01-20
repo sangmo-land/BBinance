0a7d6eacba929337c0bb8cc79f3633661bc828ad
sangmo-land <sangmolandry12@gmail.com>
2026-01-20 18:12:34 +0100

    feat: add transition component and update manifest with new asset names
    - Introduced a new transition component in `transition-CBeSVAqy.js`.
    - Updated the manifest file to reflect changes in asset names and imports for various components.
    - Ensured all references to previous asset names are replaced with the new ones across the application.

--- public/build/assets/ApplicationLogo-DERSyJZX.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./app-D0QdrOZ9.js";function r(o){return t.jsx("img",{...o,src:"/images/logo.png",alt:"Logo"})}export{r as A};
---
+++ public/build/assets/ApplicationLogo-h0gt_svX.js
@@ -0,0 +1,1 @@
+import{j as t}from"./app-B7T6_7p4.js";function r(o){return t.jsx("img",{...o,src:"/images/logo.png",alt:"Logo"})}export{r as A};
